b795d37bd130f0bd160cfef26509315928fc8bcd
shukronaasqarova <shukronaasqarova94@gmail.com>
2024-09-08 23:32:04 +0500

    end

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const photoContainer = document.getElementById('photo-container');
     const loader = document.getElementById('loader');
-
-    // Function to create a card
     function createCard(photo) {
         const card = document.createElement('div');
         card.classList.add('card');
@@ -18,11 +16,10 @@ document.addEventListener("DOMContentLoaded", function() {
         return card;
     }
 
-    // Fetch data from API
     fetch('https://jsonplaceholder.typicode.com/photos?_limit=12')
         .then(response => response.json())
         .then(photos => {
-            loader.style.display = 'none'; // Hide loader when data is loaded
+            loader.style.display = 'none'; 
             photos.forEach(photo => {
                 const card = createCard(photo);
                 photoContainer.appendChild(card);
